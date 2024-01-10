be31679ef7c4fd2d6592606b4b9ea9c25b82655a
Thanh <ge42xag@mytum.de>
2024-01-10 13:55:18 +0100

    bf in template

--- src/filter/bf/bf.hpp
+++ src/filter/bf/bf.hpp
@@ -1,21 +1,51 @@
 #pragma once
-#include <bitset>
 #include <cmath>
 
 #include "../../ultility/help.hpp"
 #include "../../ultility/sha.hpp"
 #include <vector>
 #include <array>
+#include <concepts>
+#include <iostream>
 
-class BloomFilter {
+template<std::integral T = bool>
+class BloomFilter{
    public:
     BloomFilter(size_t size);
-    size_t size();
-    std::string realSizeInMB();
-    bool add(const std::string& item);
-    bool search(const std::string& item);
+    virtual bool add(std::string_view item);
+    virtual bool lookup(std::string_view item);
+    virtual ~BloomFilter() = default;
 
    private:
     size_t _size;
-    std::vector<bool> _filter;
+    std::vector<T> _filter;
 };
+
+template<std::integral T>
+BloomFilter<T>::BloomFilter(size_t size) : _size{size}{
+    _filter.resize(_size, 0);
+}
+
+template<std::integral T>
+bool BloomFilter<T>::add(std::string_view item){
+    auto hashArray = SHA::createHashArray(static_cast<std::string>(item));
+    for (size_t i = 0; i < hashArray.size(); ++i) {
+        auto pos = hashArray.at(i) % _size;
+        if(_filter.at(pos) == 0){
+            _filter.at(pos) = 1;
+        }
+    }
+    return true;
+}
+
+template<std::integral T>
+bool BloomFilter<T>::lookup(std::string_view item){
+    auto hashArray = SHA::createHashArray(static_cast<std::string>(item));
+    for (size_t i = 0; i < hashArray.size(); ++i) {
+        auto pos = hashArray.at(i) % _size;
+        if(_filter.at(pos) == 0){
+            return false;
+        }
+    }
+    return true;
+}
