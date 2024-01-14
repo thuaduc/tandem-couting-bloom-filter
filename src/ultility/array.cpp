c40154e9a45e275d096836e4961c747ca4eed38f
thuaduc24042001 <thuaduc24042001@gmail.com>
2024-01-14 22:06:20 +0100

    create new data structure

--- src/ultility/array.cpp
+++ src/ultility/array.cpp
@@ -2,13 +2,16 @@
 
 namespace user {
 
-Array::Array(size_t size, size_t lengthOfItem) : _lengthOfItem(lengthOfItem) {
-    size_t sizePerItem = u8size / lengthOfItem;
-    size_t newSize = (size + sizePerItem - 1) / sizePerItem;
-    _vector.assign(newSize, 0);
+Array::Array(size_t size, size_t bitsPerItem) : _bitsPerItem(bitsPerItem) {
+    if (bitsPerItem != 1 && bitsPerItem != 2 && bitsPerItem != 4 &&
+        bitsPerItem != 8) {
+        std::cerr << "Bits per item muss be in [1,2,4,8]!" << std::endl;
+        exit(0);
+    }
+    _vector.assign(((size * bitsPerItem) + 7) / 8, 0);
 }
 
-void Array::set(size_t index) {
+void Array::setBitAtIndex(size_t index) {
     size_t pos = index / u8size;
     uint8_t mask = 1 << (index - (pos * u8size));
     _vector.at(pos) = _vector.at(pos) | mask;
@@ -26,6 +29,108 @@ bool Array::isSet(size_t index) {
     return (_vector.at(pos) & mask) != 0;
 }
 
+void Array::incrementCounterByOne(size_t index) {
+    if (_bitsPerItem != 4) {
+        std::cerr << "This method should only be used by couting bloom filter!"
+                  << std::endl;
+        exit(0);
+    }
+    size_t pos = index / 2;
+    if (pos >= _vector.size()) {
+        std::cerr << "Filter out of range" << std::endl;
+        exit(0);
+    }
+
+    uint8_t current, mask;
+
+    // increment high part of uint8
+    if (index % 2 != 0) {
+        current = (_vector.at(pos) & highMask) >> 4;
+        if (current == 15) {
+            std::cerr << "Counter overflow by couting bloom filter"
+                      << std::endl;
+            exit(0);
+        }
+        mask = _vector.at(pos) & lowMask;
+        _vector.at(pos) = (++current << 4) | mask;
+
+    }
+    // increment low part of uint8
+    else {
+        current = _vector.at(pos) & lowMask;
+        if (current == 15) {
+            std::cerr << "Counter overflow by couting bloom filter"
+                      << std::endl;
+            exit(0);
+        }
+        mask = _vector.at(pos) & highMask;
+        _vector.at(pos) = ++current | mask;
+    }
+}
+
+bool Array::decrementCounterByOne(size_t index) {
+    if (_bitsPerItem != 4) {
+        std::cerr << "This method should only be used by couting bloom filter!"
+                  << std::endl;
+        exit(0);
+    }
+    if ((index / 2) >= _vector.size()) {
+        std::cerr << "Filter out of range" << std::endl;
+        exit(0);
+    }
+
+    size_t pos = index / 2;
+    uint8_t current, mask;
+
+    // decrement high part of uint8
+    if (index % 2 != 0) {
+        current = (_vector.at(pos) & highMask) >> 4;
+        if (current == 0) {
+            return false;
+        }
+        mask = _vector.at(pos) & lowMask;
+        _vector.at(pos) = (--current << 4) | mask;
+
+    }
+    // decrement low part of uint8
+    else {
+        current = _vector.at(pos) & lowMask;
+        if (current == 0) {
+            return false;
+        }
+        mask = _vector.at(pos) & highMask;
+        _vector.at(pos) = --current | mask;
+    }
+
+    return true;
+}
+
+void Array::incrementCounter(size_t index, uint8_t num) {
+    if (_vector.at(index) + num >= UINT8_MAX) {
+        std::cerr << "Counter overflow by variable couting bloom filter"
+                  << std::endl;
+    }
+    if (index >= _vector.size()) {
+        std::cerr << "Filter out of range" << std::endl;
+        exit(0);
+    }
+
+    _vector.at(index) += num;
+}
+
+bool Array::decrementCounter(size_t index, uint8_t num) {
+    if (_vector.at(index) == 0 || _vector.at(index) < num) {
+        return false;
+    }
+    if (index >= _vector.size()) {
+        std::cerr << "Filter out of range" << std::endl;
+        exit(0);
+    }
+
+    _vector.at(index) -= num;
+    return true;
+}
+
 void Array::printBits(uint8_t value) {
     for (int i = 7; i >= 0; --i) {
         uint8_t bit = (value & (1 << i)) ? 1 : 0;
@@ -36,7 +141,9 @@ void Array::printBits(uint8_t value) {
 void Array::print() {
     for (const auto& value : _vector) {
         printBits(value);
+        printf(" ");
     }
+    printf("\n");
 }
 
 size_t Array::size() { return _vector.size(); }
--- src/ultility/array.hpp
+++ src/ultility/array.hpp
@@ -3,20 +3,39 @@
 #include <vector>
 
 #define u8size 8
-
+#define lowMask 15    // 0b00001111
+#define highMask 240  // 0b11110000
 namespace user {
 
 class Array {
    public:
-    Array(size_t size, size_t lengthOfItem);
+    Array(size_t size, size_t _bitsPerItem);
 
     size_t size();
 
-    void set(size_t index);
+    // set bit at given index to 1
+    // only for standard bloom filter
+    void setBitAtIndex(size_t index);
 
+    // check if bit is set at given index
+    // only for standard bloom filer
     bool isSet(size_t index);
 
-    // void increase(uint8_t num);
+    // increment filter element at given index by one
+    // only for counting bloom filter
+    void incrementCounterByOne(size_t index);
+
+    // decrement filter element at given index by one
+    // only for counting bloom filter
+    bool decrementCounterByOne(size_t index);
+
+    // increment filter element at given index by given amount
+    // only for variable couting bloom filter
+    void incrementCounter(size_t index, uint8_t num);
+
+    // increment filter element at given index by given amount
+    // only for variable couting bloom filter
+    bool decrementCounter(size_t index, uint8_t num);
 
     void printBits(uint8_t value);
 
@@ -24,7 +43,7 @@ class Array {
 
    private:
     std::vector<uint8_t> _vector;
-    size_t _lengthOfItem;
+    size_t _bitsPerItem;
 };
 
 }  // namespace user
