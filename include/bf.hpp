5c4d0d5dc506e358436319233c5699d9d4a1d7c2
Thanh <ge42xag@mytum.de>
2024-01-27 19:39:08 +0100

    remove unnecessary destructor

--- include/bf.hpp
+++ include/bf.hpp
@@ -10,8 +10,7 @@ class BloomFilter {
     BloomFilter(size_t m, uint8_t k);
     void insert(uint8_t *key, uint16_t keyLength);
     bool lookup(uint8_t *key, uint16_t keyLength);
-    ~BloomFilter() = default;
-
+    
    private:
     std::vector<std::function<uint64_t(uint8_t* key, uint16_t keyLength)>> f_set;
     std::vector<uint8_t> filter;
--- include/cbf.hpp
+++ include/cbf.hpp
@@ -14,7 +14,6 @@ class CountingBloomFilter {
     void insert(uint8_t *key, uint16_t keyLength);
     bool lookup(uint8_t *key, uint16_t keyLength);
     bool remove(uint8_t *key, uint16_t keyLength);
-    ~CountingBloomFilter() = default;
 
    private:
     std::vector<std::function<uint64_t(uint8_t *key, uint16_t keyLength)>> f_set;
